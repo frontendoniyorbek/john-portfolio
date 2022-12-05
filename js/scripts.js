319410ccb803a81c35215bfae43838e1b1695095
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-12-05 16:31:23 +0500

    img, header page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 const nav = document.querySelector('nav')
 const toggleBtn = document.querySelector('.toggele-btn')
-// const toggleBtnImg = document.querySelector('.toggele-btn img')
+const toggleBtnImg = document.querySelector('.toggele-btn img')
 
 toggleBtn.addEventListener('click', () => {
     nav.classList.toggle('hidden')
+
+    if (nav.classList.contains('hidden')) {
+        toggleBtnImg.src = '../images/svg/hamburger-icon.svg'
+    } else {
+        toggleBtnImg.src = '../images/svg/close-icon.svg'
+    }
 })
